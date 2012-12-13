6e9a0f376aadb748a9a7111c18a1e6a60f919aca
Essa <qnix@0x80.org>
2012-12-13 08:04:42 -0800

    Loading proxy info from the configuration file

--- src/twitterfu.cpp
+++ src/twitterfu.cpp
@@ -90,6 +90,18 @@ bool config(string filename, User * user)
 	if (conf["unfollowed"].empty())
 		return false;
 	user->cache.unfollowed = conf["unfollowed"];
+        if(conf["proxy_username"].empty())
+                return false;
+        user->proxy.username = conf["proxy_username"];
+        if(conf["proxy_password"].empty())
+                return false;
+        user->proxy.password = conf["proxy_password"];
+        if(conf["proxy_address"].empty())
+                return false;
+        user->proxy.address = conf["proxy_address"];
+        if(conf["proxy_port"].empty())
+                return false;
+        user->proxy.port = conf["proxy_port"]; 
 
 	return true;
 }
